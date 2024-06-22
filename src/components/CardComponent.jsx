1f5c664c2ebfd7bafd2512c569dbb422a3357fe6
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2024-06-22 13:04:54 +0530

    search_page:feat

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -8,12 +8,13 @@ function CardComponent({item,type}) {
   
   return (
     <Link  href={`${type}/${item?.id}`} underline="none">
-      <Box className="MovieTitle">
-      <Box className="MovieBox" component="section" >
-        <img className="MovieImg"   src={`${imagePath}/${item?.poster_path}`}  alt={item?.title || item?.name}/>
+      <Box className="MovieTitle" style={{background:"transparent" ,border:"none",height:"205px",width:"200px",marginBottom:"10px"}}>
+      <Box className="MovieBox" component="section"  >
+        <img className="MovieImg"   srcset={`${imagePath}/${item?.poster_path}`}  alt={item?.title || item?.name}/>
+        
       </Box>
       <Box  className="MovieDetails">
-        <h4  style={{color:'white' ,fontSize:14 ,fontFamily:"monospace",fontWeight:"bold",textAlign:"center", justifyItem:"center"}}>{item?.title ||item?.name}</h4>
+        <h4  style= {{color:'white' ,fontSize:14 ,fontFamily:"monospace",fontWeight:"bold",textAlign:"center", justifyItem:"center"}}>{item?.title ||item?.name}</h4>
         <h4  style={{color:'white' ,fontSize:14 ,fontFamily:"monospace",fontWeight:"normal",textAlign:"center", justifyItem:"center"}}> { new Date(item?.release_date ||item?.first_air_date).getFullYear() || "N/A"}</h4>
          <Stack direction={"row"} justifyContent={'center'} gap={1}  marginBottom={1}>
           <img style={{height:20,width:20}}src="../src/assets/star.png" />
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,23 +1,67 @@
-import { Stack } from '@mui/material';
+import { CircularProgress, Grid, Skeleton, Stack } from '@mui/material';
 import Container from '@mui/material/Container';
 import React, { useEffect, useState } from 'react';
+import CardComponent from '../../components/CardComponent';
+import Pagination from '../../components/Pagination';
 import { fetchSearch } from '../../services/api';
 import '../movies.css';
+
+
 function Search() {
   const [searchValue,setSearchValue] = useState("");
+  const [totalPages,setTotalPages] = useState(1);
   const [activepage,setactivepage] = useState(1);
-  useEffect(()=>{},[]);
+  const [loading,setisLoading]=useState(false);
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    setisLoading(true);
+    fetchSearch(activepage,searchValue)
+    .then((res)=>{
+      console.log(res);
+      setData(res?.results);
+      setactivepage(res?.page);
+      setTotalPages(res?.total_pages);
+    })
+    .catch((err)=>{console.log(err)}).finally(()=>{setisLoading(false)})
+  
+  },[activepage,searchValue])
+
   const handleSearch=(e)=>{
     e.preventDefault();
-    fetchSearch(searchValue,activepage).then((res)=>{console.log(res)}).catch((err)=>{console.log(err)})
-  }
+    setSearchValue(searchValue);
+   }
+ 
   return (
     <Container sx={{ml:5}}>
       <Stack   direction="row"  alignItems="baseline" justifyContent="space-between" >  
       <h2 style={{ fontSize:"larger", fontWeight:"normal" }}>SEARCH</h2></Stack>
       <form onSubmit={handleSearch}>
-        <input type="text"  value={searchValue} placeholder='Search movies, tv shows' onChange={(e)=>{setSearchValue(e.target.value)}}/>
+        <input type="text"  value={searchValue} 
+        placeholder='Search movies, tv shows' 
+        onChange={(e)=>{setSearchValue(e.target.value)}}/>
       </form>
+      {loading && (<Stack justifyContent={'center'}  alignItems={'center'} marginTop={10}>
+        <CircularProgress style={{justifyContent:"center" ,alignItems:"center",color:"red"}}/>
+      </Stack>)}
+      {data?.length===0 && loading && (
+              <h4 style={{fontSize:20 ,color:"white.100",textAlign:"center",marginTop:"100px"}}>
+                No results found</h4>)}
+      <Grid className='GridBox' container   
+      gridTemplateColumns={{base:"repeat(2,1fr)",sm:"repeat(2,1fr)",md:"repeat(3,1fr)",
+      lg:"repeat(5,1fr)"}} justifyContent="space-between" alignItems="center">
+        {data?.length>0 && !loading && data?.map((item,i)=>{
+          return loading? (<Skeleton key={i}  sx={{ bgcolor: 'grey.900' }} 
+            className='Skeleton' variant="rectangular" width={200} height={500} bgcolor="grey"/>) 
+            :(<CardComponent  key={item?.id} item={item} type={item?.media_type}/>
+              
+            )
+        })}
+         </Grid>
+         {data?.length>0 && !loading &&
+         <Pagination activepage={activepage} totalPages={totalPages} setActivePage={setactivepage}/>}
+       
+        
+  
     </Container>
   )
 }
